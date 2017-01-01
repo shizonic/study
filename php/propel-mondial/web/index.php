4738e75adae381526852689531f88ffe154fbad2
Thomas Breuss <thomasbreuss@gmx.ch>
2017-01-01 17:00:07 +0100

    chmod +x shell scripts, excluded log dir

--- php/propel-mondial/web/index.php
+++ php/propel-mondial/web/index.php
@@ -9,6 +9,6 @@ require_once __DIR__ . '/../generated-conf/config.php';
 use Model\CountryQuery;
 
 $countries = CountryQuery::create()->find();
-foreach($countries as $country) {
-  echo $country->getName() . "<br>";
+foreach ($countries as $country) {
+    echo $country->getName() . "<br>";
 }
