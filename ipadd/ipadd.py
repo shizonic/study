7368004bd4ddb5a0178abfa83c1f1bd3224f53a8
Thomas Breuss <thomasbreuss@gmx.ch>
2017-01-04 06:00:35 +0100

    deleted duplicated content

--- ipadd/ipadd.py
+++
@@ -1,6 +0,0 @@
-ipaddr = '192.168.10.33'
-
-print((192 << 24) + (168 << 16) + (10 << 8) + 33)
-print(sum([(n << 8 * i) for i, n
-           in enumerate(reversed([int(n)
-                                  for n in ipaddr.split('.')]))]))
