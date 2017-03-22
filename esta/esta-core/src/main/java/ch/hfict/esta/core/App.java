1170af65330f3c3712eb32638220466f5127faee
Thomas Breuss <thomasbreuss@gmx.ch>
2017-03-22 20:25:11 +0100

    Added logger and checkstyle (not working at the moment)

--- esta/esta-core/src/main/java/ch/hfict/esta/core/App.java
+++ esta/esta-core/src/main/java/ch/hfict/esta/core/App.java
@@ -6,21 +6,34 @@ import javax.persistence.Persistence;
 import ch.hfict.esta.core.repository.StudentJpaRepository;
 import ch.hfict.esta.core.domain.Student;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    final static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ch.hfict.esta");
         StudentJpaRepository studentRepository = new StudentJpaRepository();
         studentRepository.setEntityManager(emf.createEntityManager());
 
+        logger.debug("Debug");
+        logger.info("Info");
+        logger.warn("Warning");
+        logger.error("Error");
+        logger.trace("Trace");
+
         Student student = new Student();
         student.setFirstname("Hans");
         student.setLastname("Muster");
+
+        System.out.println( "ID before persist: " + student.getId());
 
         student = studentRepository.save(student);
 
